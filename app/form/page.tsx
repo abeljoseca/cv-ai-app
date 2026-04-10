aa38d9cb951b78cb8767f748e687555367aa2df2
abeljoseca <abeljoseca@gmail.com>
2026-04-10 19:59:35 -0400

    fix: corregir paginas viejas que causaban error en Vercel

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,25 +1,5 @@
-"use client";
-
-import { supabase } from "@/lib/supabase";
-import { useEffect } from "react";
+import { redirect } from 'next/navigation'
 
 export default function FormPage() {
-  useEffect(() => {
-    const testConnection = async () => {
-      const { data, error } = await supabase.from("test").select("*");
-
-      console.log("DATA:", data);
-      console.log("ERROR:", error);
-    };
-
-    testConnection();
-  }, []);
-
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">
-        Conectando con Supabase... 🧠
-      </h1>
-    </main>
-  );
+  redirect('/login')
 }
